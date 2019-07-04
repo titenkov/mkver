a17d18a267eefa13a14ed7a499ef187ccc17fa17
paveltitenkov <paveltitenkov@coherentsolutions.com>
2019-07-04 12:35:38 +0300

    clean-up

--- mkver.go
+++ mkver.go
@@ -19,7 +19,7 @@ func main() {
 	app := cli.NewApp()
 
 	app.Name = "mkver"
-	app.Usage = "Calculates semantic version by enriching the original one with various aspects"
+	app.Usage = "Calculates application version by enriching the original one with various information"
 	app.Version = "0.3.0"
 	app.Commands = nil
 	app.Flags = []cli.Flag{
@@ -54,8 +54,55 @@ func main() {
 	}
 }
 
+// Calculate produces application version by enriching the original one with meta-informaiton based on the provided flags
+func Calculate(ctx cli.Context) (string, error) {
+	var versionBuilder strings.Builder
+
+	// Resolve the version, that will be used as a ground for further calculations
+	// Version can be resolved from the env variable, gradle.properties or any other supported location
+	version, err := resolveVersion(&ctx)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Splits original version by "-" into 2 parts: root and ext. F.e. 1.0.0-SNAPSHOT => 1.0.0 (root) and SNAPSHOT (ext)
+	versionRoot, versionExt := resolveVersionRootAndExt(version)
+
+	versionBuilder.WriteString(versionRoot)
+
+	// Resolve the git branch
+	branch, err := resolveGitBranch(&ctx)
+
+	if err != nil || len(branch) == 0 {
+		branch = "unknown"
+	}
+
+	// Process git-ref. F.e. 1.0.0-SNAPSHOT on feature/x branch => 1.0.0-feature-x-SNAPSHOT
+	processGitRef(&ctx, branch, &versionBuilder)
+
+	// Process git-build-num. Will add build number taken from env variable to the result version.
+	// F.e. 1.0.0 on the release/1.0.0 branch => 1.0.0-rcX (where x is a $BUILD_NUMBER env variable)
+	processGitBuildNum(&ctx, branch, &versionBuilder)
+
+	// Process git-sha. Will add git sha to the result version.
+	// F.e. 1.0.0-SNAPSHOT => 1.0.0-ea3op1-SNAPSHOT
+	processGitSha(&ctx, branch, &versionBuilder)
+
+	// Appending back the version extension, which has been calculated together with a version root
+	if len(versionExt) > 0 {
+		versionBuilder.WriteString("-" + versionExt)
+	}
+
+	return versionBuilder.String(), nil
+}
+
+//
+// UTILS
+//
+
 // Resolves original version from one of the sources
-func resolveVersion(ctx cli.Context) (string, error) {
+func resolveVersion(ctx *cli.Context) (string, error) {
 
 	// Resolve from the provided env variable
 	// In case, if "--env=.." flag is specified, resolve env variable with the provided name
@@ -104,7 +151,16 @@ func resolveVersion(ctx cli.Context) (string, error) {
 	return "", errors.New("Failed to resolve version")
 }
 
-func resolveGitBranch(ctx cli.Context) (string, error) {
+func resolveVersionRootAndExt(version string) (string, string) {
+	if strings.Contains(version, "-") {
+		versionParts := strings.Split(version, "-")
+		return strings.TrimSpace(versionParts[0]), strings.TrimSpace(versionParts[1])
+	}
+
+	return version, ""
+}
+
+func resolveGitBranch(ctx *cli.Context) (string, error) {
 	// Determine the git branch from env if running on CI, otherwise from git
 	if _, found := os.LookupEnv("BUILD_NUMBER"); found { // magic jenkins variable
 		if _, found := os.LookupEnv("CHANGE_ID"); found { // Are we building a PR?
@@ -122,53 +178,32 @@ func resolveGitBranch(ctx cli.Context) (string, error) {
 	return strings.TrimSpace(string(out[:])), err
 }
 
-// Calculate produces the semantic version according to the provided flags
-func Calculate(ctx cli.Context) (string, error) {
-	var versionBuilder strings.Builder
-	var versionRoot, versionExt string
+func processGitRef(ctx *cli.Context, branch string, versionBuilder *strings.Builder) {
 
-	// Resolve the version, that will be used as a ground for further calculations
-	version, err := resolveVersion(ctx)
-
-	if err != nil {
-		return "", err
+	// Check if "--git-ref" flag is specified, otherwise - skip version processing
+	if !ctx.IsSet(flags.GitRefFlag.Name) || !ctx.Bool(flags.GitRefFlag.Name) {
+		return
 	}
 
-	if strings.Contains(version, "-") {
-		versionRoot = strings.TrimSpace(strings.Split(version, "-")[0])
-		versionExt = strings.TrimSpace(strings.Split(version, "-")[1])
-	} else {
-		versionRoot = version
-		versionExt = ""
-	}
+	var ignore bool
 
-	versionBuilder.WriteString(versionRoot)
+	if ctx.IsSet(flags.GitRefIgnoreFlag.Name) {
+		var ignoreBranches = ctx.StringSlice(flags.GitRefIgnoreFlag.Name)
 
-	// Resolve the git branch
-	branch, err := resolveGitBranch(ctx)
-
-	if err != nil || len(branch) == 0 {
-		branch = "unknown"
-	}
-
-	if ctx.IsSet(flags.GitRefFlag.Name) && ctx.Bool(flags.GitRefFlag.Name) {
-		var ignore bool
-
-		if ctx.IsSet(flags.GitRefIgnoreFlag.Name) {
-			var ignoreBranches = ctx.StringSlice(flags.GitRefIgnoreFlag.Name)
-
-			for _, b := range ignoreBranches {
-				if match, _ := regexp.MatchString(b, branch); match {
-					ignore = true
-				}
+		for _, b := range ignoreBranches {
+			if match, _ := regexp.MatchString(b, branch); match {
+				ignore = true
 			}
 		}
-
-		if !ignore {
-			versionBuilder.WriteString("-" + strings.ToLower(strings.Replace(branch, "/", "-", -1)))
-		}
 	}
 
+	if !ignore {
+		versionBuilder.WriteString("-" + strings.ToLower(strings.Replace(branch, "/", "-", -1)))
+	}
+
+}
+
+func processGitBuildNum(ctx *cli.Context, branch string, versionBuilder *strings.Builder) {
 	if ctx.IsSet(flags.GitBuildNumFlag.Name) {
 		var ignore = false
 
@@ -193,23 +228,15 @@ func Calculate(ctx cli.Context) (string, error) {
 			versionBuilder.WriteString("-" + ctx.String(flags.GitBuildNumFlag.Name) + buildNumber)
 		}
 	}
+}
 
+func processGitSha(ctx *cli.Context, branch string, versionBuilder *strings.Builder) {
 	if ctx.IsSet(flags.GitShaFlag.Name) && ctx.Bool(flags.GitShaFlag.Name) {
 		out, _ := exec.Command("bash", "-c", "git rev-parse --short=6 HEAD 2> /dev/null  || echo 'unknown'").Output()
 		sha := strings.TrimSpace(string(out[:]))
 		versionBuilder.WriteString("-" + sha)
 	}
-
-	if len(versionExt) > 0 {
-		versionBuilder.WriteString("-" + versionExt)
-	}
-
-	return versionBuilder.String(), nil
 }
-
-//
-// UTILS
-//
 
 func readPropertiesFile(filename string) (map[string]string, error) {
 	config := map[string]string{}
